fc3007092870a72aa9c2278d860b591ddcd85540
lowyanyu <lyyyanyu@gmail.com>
2022-01-02 23:36:35 +0800

    login prevent dblClick, 404 unauthorized page 20210102

--- src/app/modules/home/pages/login/login.component.ts
+++ src/app/modules/home/pages/login/login.component.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { NgCryptoService } from '@cg/ng-crypto';
 import { LoginService } from '@home/services/login.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   loginForm: FormGroup;
 
-  alertMsg: string;
+  loginStatus = 0;
+  loginSubscription: Subscription;
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
     private loginService: LoginService,
+    private snackBar: MatSnackBar,
     private crypto: NgCryptoService
   ) {
     this.loginForm = this.fb.group({
@@ -30,19 +34,32 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+    this.snackBar.dismiss();
+    if (this.loginSubscription) {
+      this.loginSubscription.unsubscribe();
+    }
+  }
+
   login(): void {
     if (this.loginForm.invalid) {
       return;
     }
 
+    this.loginStatus = 1;
     const value = this.loginForm.value;
-    this.loginService.login(value.account, this.crypto.sha256(value.pwd)).subscribe({
-      next: data => {
-        this.alertMsg = '登入成功';
+    this.loginSubscription = this.loginService.login(value.account, this.crypto.sha256(value.pwd)).subscribe({
+      next: () => {
+        this.loginStatus = 0;
         this.router.navigate(['/main']);
       },
       error: error => {
-        this.alertMsg = '登入失敗！' + ( error.errorMessage || '' );
+        this.loginStatus = 0;
+        const msg = '登入失敗！' + ( error.errorMessage || '請聯絡全景娛樂城管理者' );
+        this.snackBar.open(msg, '知道了', {
+          duration: 2000,
+          panelClass: 'my-snackbar'
+        });
       }
     })
   }
--- src/app/pages/unauthorized/unauthorized.component.ts
+++ src/app/pages/unauthorized/unauthorized.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Subscription, timer } from 'rxjs';
+import { filter, takeWhile, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-unauthorized',
   templateUrl: './unauthorized.component.html',
   styleUrls: ['./unauthorized.component.scss']
 })
-export class UnauthorizedComponent implements OnInit {
+export class UnauthorizedComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  timer$ = new BehaviorSubject<number>(4);
+  timerSubscription: Subscription;
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.timerSubscription = timer(100, 1000).pipe(
+      takeWhile(() => this.timer$.value > 0),
+      tap(() => this.timer$.next(this.timer$.value - 1)),
+      filter(() => this.timer$.value === 0)
+    ).subscribe({
+      next: () => {
+        this.backToHome();
+      }
+    });
+  }
+
+  backToHome(): void {
+    this.router.navigate(['home']);
+  }
+
+  ngOnDestroy(): void {
+    this.timerSubscription.unsubscribe();
   }
 
 }
